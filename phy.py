76bff3c456b8cae86bf4c7d76d078af424f105a4
svenkat143 <56305736+svenkat143@users.noreply.github.com>
2019-10-09 21:04:38 +0530

    Update phy.py

--- phy.py
+++ phy.py
@@ -62,10 +62,3 @@ for c in ime:
     elif (c == "."):
         print("----..----\n\n")
 
-print("______________________________________")
-print("If you like it!!! Please hit one like.")
-print("______________________________________")
-print("Thanks for all the likes people! I have made this in Delphi for you")
-print("There is a repo in Bitbucket, so if you want you can compile it yourself")
-print("Or you can download exe just for fun :)")
-print("REPO: https://kordarei@bitbucket.org/kordarei/solo_magic.git")
